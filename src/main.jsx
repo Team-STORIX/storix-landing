60634fde7e191e2d0106b6d72c1dbf179e2ef52b
codus02 <chaetree02@gmail.com>
2026-07-31 17:36:10 +0900

    Update landing download buttons

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,28 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 
-const INSTALL_URL = 'https://truth-gopher-09e.notion.site/35de81f7094880bba04fd4eaef2d5e42'
+const IOS_DOWNLOAD_URL = 'https://apps.apple.com/kr/app/%EC%8A%A4%ED%86%A0%EB%A6%AD%EC%8A%A4-storix/id6762126035'
+const ANDROID_DOWNLOAD_URL = 'https://play.google.com/store/apps/details?id=kr.storix.android'
 
 function App() {
   return (
     <main className="landing">
-      <picture className="landingPicture">
-        <source srcSet="/mobile.svg" media="(max-width: 767px)" />
-        <img className="landingImage" src="/pc.svg" alt="" />
-      </picture>
-      <a
-        className="downloadHotspot"
-        href={INSTALL_URL}
-        aria-label="앱 다운로드하기"
-      />
+      <div className="landingFrame">
+        <picture className="landingPicture">
+          <source srcSet="/mobile.png" media="(max-width: 767px)" />
+          <img className="landingImage" src="/pc.png" alt="" />
+        </picture>
+        <a
+          className="downloadHotspot ios"
+          href={IOS_DOWNLOAD_URL}
+          aria-label="iOS download"
+        />
+        <a
+          className="downloadHotspot android"
+          href={ANDROID_DOWNLOAD_URL}
+          aria-label="Android download"
+        />
+      </div>
     </main>
   )
 }
