f13eb382c97d4a871ba9f3f4395eaa60e0118b19
Tutankhannun <c335098@g.hongik.ac.kr>
2026-08-17 05:27:13 +0900

    feat: Add story card event layout

--- src/pages/StoryCardEventPage.jsx
+++ src/pages/StoryCardEventPage.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react'
 import '../story-card-event.css'
 
 export default function StoryCardEventPage() {
+  useEffect(() => {
+    const previousTitle = document.title
+    document.title = '스토리카드 이벤트 | STORIX'
+    document.documentElement.classList.add('storyCardDocument')
+
+    return () => {
+      document.title = previousTitle
+      document.documentElement.classList.remove('storyCardDocument')
+    }
+  }, [])
+
   return (
     <main className="storyCardEventPage">
       <h1>스토리카드 이벤트입니다</h1>
